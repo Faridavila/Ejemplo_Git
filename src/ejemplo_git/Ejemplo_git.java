07dc2980b5fb27b6650e2406c1f1b3a04799cb6e
Farid Avila <farid.avila01@uceva.edu.co>
2022-04-04 19:45:36 -0500

    Se hizo una impresion de hola mundo

--- src/ejemplo_git/Ejemplo_git.java
+++ src/ejemplo_git/Ejemplo_git.java
@@ -14,7 +14,7 @@ public class Ejemplo_git {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.err.println("Hola mundo");
     }
     
 }
